52ccb68fde0e7274b71db0c7b56c40c5e1e7824b
sgerodes <sgerodes@gmail.com>
2022-06-11 21:21:51 +0200

    adds update trust

--- src/components/Buttons/UpdateTrustButton.tsx
+++ src/components/Buttons/UpdateTrustButton.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react'
+import {Button} from "@mantine/core";
+import Link from 'next/link';
 
 const UpdateTrustButton: FC = () => {
 
     return (
-        <>
-
-        </>
+        <Link href="/update/1" >
+            <Button>
+                Update
+             </Button>
+        </Link>
   );
 };
 
